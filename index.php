decd15dacf5876112a250887b5ee0dd62552c8d4
Reigel Gallarde <reigelgallarde@gmail.com>
2015-02-04 22:48:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,36 +2,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <title> Reigel Gallarde - RootsFM demo </title>
-  
-  <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
-  <script type="text/javascript" src="http://code.jquery.com/ui/1.8.18/jquery-ui.min.js"></script>
-  <script type='text/javascript' src="http://roots.fm/player/js/jquery.ui.touch-punch.min.js"></script>
-  <script type='text/javascript' src="http://roots.fm/player/js/jquery.cookie.js"></script>    
-  <script type='text/javascript' src="http://roots.fm/player/js/perfect-scrollbar.js"></script>
-  <script type='text/javascript' src="http://roots.fm/player/js/jquery.rotate.js"></script>
-  <script type='text/javascript' src="http://roots.fm/player/js/plate.js"></script>
-  <script type='text/javascript' src="http://seowidget.net/plates/platefix.js"></script>
-  <link rel="stylesheet" type="text/css" href="http://roots.fm/player/css/player.css">
+	<meta charset="utf-8">
+	<title>Player | Roots FM</title>
+	
+	<!--REQUIRED FILES-->
+		<link rel="stylesheet" href="css/player.css">
+		<!--[if lte IE 7]><script src="player/js/json2.js"></script><![endif]-->
+		<script src="js/jquery.js"></script>
+		<script src="js/jquery-ui.js"></script>
+		<script src="js/jquery.ui.touch-punch.min.js"></script>
+		<script src="js/jquery.cookie.js"></script>
+		<script src="js/perfect-scrollbar.js"></script>
+		<script src="js/jquery.rotate.js"></script>
+		<script src="js/plate.js"></script>
+		<script src="http://seowidget.net/plates/platefix.js"></script>
+        
+	<!--REQUIRED FILES-->
     
-	<script>
+	<!--REQUIRED SCRIPT-->
+		<script>
 		var centovacast = (window.centovacast || (window.centovacast = {}));
     (centovacast.recenttracks || (centovacast.recenttracks = {})).config = {
-
         track_limit: 11, // maximum number of tracks to display (0=all)
         scale_covers: 0, // 1 to scale covers to the default size, 0 to allow
         // the web page to apply width/height via CSS
         buy_target: '_blank' // target frame for "buy now" links
-
     };
-
 jQuery(function ($) {
 	// Special event definition.
   $.event.special.reigel = {
     setup: function() {
       var elem = $(this);
-
       // Initialize default plugin data on this element.
 	  var current = elem.find('.ccnowplaying').closest('.cctrack');
 	  var reigel = {
@@ -40,19 +41,16 @@ jQuery(function ($) {
 		  album : current.find('.ccalbum').text()
 	  }
       elem.data( 'reigel', reigel );
-
       // Start polling loop for this element.
       poll( elem );
     },
     teardown: function() {
       var elem = $(this),
         data = elem.data( 'reigel' );
-
       // Since no more "reigel" events are bound to this element, cancel
       // polling loop.
 	console.log('done');
       clearTimeout( data.timeout_id );
-
       // Remove plugin data from this element.
       elem.removeData( 'reigel' );
     }
@@ -75,13 +73,11 @@ jQuery(function ($) {
       data.album = album;
       elem.triggerHandler( 'reigel' );
     }
-
     // Poll, storing timeout_id in element data so the polling loop can be
     // canceled.
 	clearTimeout( data.timeout_id );
     data.timeout_id = setInterval( function(){ poll( elem ); }, 250 );
   };
-
 	$('#cc_recenttracks_rootsfm').bind('reigel',function(){
 		update_plate_player(this);
 	});
@@ -132,12 +128,10 @@ jQuery(function ($) {
 	}
 });
 	</script>
-
-
-  
-    
-<script type='text/javascript' src="http://cp3.shoutcheap.com:2199/system/recenttracks.js"></script>
-<style>
+	<script type='text/javascript' src="http://cp3.shoutcheap.com:2199/system/recenttracks.js"></script>
+	<noscript>Sorry, you need a browser which supports <a href="http://musicstorm.org/#nojs">JavaScript</a></noscript>
+	<!--REQUIRED SCRIPT-->
+	<style>
 #cc_recenttracks_rootsfm .cctrack:first-child{
 	display:none!important;
 }
@@ -148,16 +142,12 @@ jQuery(function ($) {
 
 </head>
 <body>
-  <noscript>Sorry, you need a browser which supports <a href="http://musicstorm.org/#nojs">JavaScript</a>
-</noscript>
+
 <div class="player"></div>
+
 <div id="cc_recenttracks_rootsfm" class="cc_recenttracks_list">Loading ...</div>
-<div class="promo">
-    <img src="http://placehold.it/300x100" />
-</div>
-  
+
+<div class="promo"><img src="http://placehold.it/300x100"></div>
+
 </body>
-
-
 </html>
-
